5a8b95336a62c6c8905a62dde2b4d82c3cc1f95f
Afsarsoft <admin@afsarsoft.com>
2022-08-07 11:31:28 -0700

    ...

--- 405-links1/src/App.vue
+++ 405-links1/src/App.vue
@@ -24,17 +24,17 @@ export default {
       <h1>Links</h1>
     </header>
     <main id="link">
-      <section id="user-goal">
+      <div class="user-goal">
         <h2>Vue Links</h2>
         <p>{{ goal }}</p>
-        <!-- We can hard code the link but let's get it dynamically -->
-        <!-- here we need directive v-bind, to bind an set the element -->
+        <!-- We can hard code the link but let's get it dynamically. -->
+        <!-- Here we need directive v-bind, to bind an set the element -->
         <!-- Basically, V-bind sets the value of an attribute. -->
         <p>Vue <a v-bind:href="vueSite" target="_blank">main site</a>.</p>
         <!-- Short cut for V-bind is : -->
         <p>Vue <a :href="vueSchool" target="_blank">school</a>.</p>
         <p>Vue <a :href="vueMastery" target="_blank">mastery</a>.</p>
-      </section>
+      </div>
     </main>
   </body>
 </template>
