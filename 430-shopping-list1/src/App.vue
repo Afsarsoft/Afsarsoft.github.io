0863a72fb91a514eebff7afd75587d17eed732b8
Afsarsoft <admin@afsarsoft.com>
2022-08-08 16:45:09 -0700

    ...

---
+++ 430-shopping-list1/src/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<template>
+  <head>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+
+  <body></body>
+</template>
+
+<style scoped></style>
