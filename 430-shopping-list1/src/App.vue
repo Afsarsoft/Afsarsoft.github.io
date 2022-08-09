f37bce6cbb4cab7f2faa9ee017df8e0568050358
Afsarsoft <admin@afsarsoft.com>
2022-08-09 13:56:29 -0700

    ...

--- 430-shopping-list1/src/App.vue
+++ 430-shopping-list1/src/App.vue
@@ -1,18 +1,117 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
-export default defineComponent({});
+export default defineComponent({
+  data() {
+    return {
+      state: "default",
+      header: "shopping list",
+      newItem: "",
+      items: [
+        {
+          label: "Banana",
+          purchased: false,
+        },
+        {
+          label: "Carrot",
+          purchased: false,
+        },
+        {
+          label: "Oatmeal",
+          purchased: true,
+        },
+        {
+          label: "Apple",
+          purchased: false,
+        },
+        {
+          label: "Avacado",
+          purchased: false,
+        },
+        {
+          label: "Bread",
+          purchased: false,
+        },
+      ],
+    };
+  },
+  // when we need to change the data, we use method
+  // When we need to change the presentation of existing data, we use computed properties
+  computed: {
+    characterCount() {
+      return this.newItem.length;
+    },
+    reversedItems() {
+      return this.items.slice(0).reverse();
+    },
+  },
+  methods: {
+    saveItem() {
+      this.items.push({
+        label: this.newItem,
+        purchased: false,
+      });
+      this.newItem = "";
+    },
+    changeState(newState: string) {
+      this.state = newState;
+      this.newItem = "";
+    },
+    togglePurchased(item: { label: string; purchased: boolean }) {
+      item.purchased = !item.purchased;
+    },
+  },
+});
 </script>
 
 <template>
-  <head>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap"
-      rel="stylesheet"
-    />
-  </head>
-
-  <body></body>
+  <body>
+    <main id="shopping">
+      <div id="shopping-list">
+        <div class="header">
+          <h1>{{ header.toLocaleUpperCase() }}</h1>
+          <button
+            v-if="state === 'default'"
+            class="btn btn-primary"
+            @click="changeState('edit')"
+          >
+            Add Item
+          </button>
+          <button v-else class="btn btn-cancel" @click="changeState('default')">
+            Cancel
+          </button>
+        </div>
+        <div v-if="state === 'edit'" class="add-item-form">
+          <input
+            v-model="newItem"
+            type="text"
+            placeholder="Add an item"
+            @keyup.enter="saveItem"
+          />
+          <p>{{ characterCount }}/200</p>
+          <button
+            class="btn btn-primary"
+            :disabled="newItem.length === 0"
+            @click="saveItem"
+          >
+            Save Item
+          </button>
+        </div>
+        <ul>
+          <li
+            v-for="item in reversedItems"
+            :class="{ strikeout: item.purchased }"
+            @click="togglePurchased(item)"
+          >
+            {{ item.label }}
+          </li>
+        </ul>
+        <p v-if="items.length === 0">
+          Nice job! You have bought all your items.
+        </p>
+      </div>
+    </main>
+  </body>
 </template>
 
 <style scoped></style>
---
+++ z-Template1/src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {};
+  },
+  methods: {},
+});
+</script>
+
+<template>
+  <head>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+
+  <body></body>
+</template>
+
+<style scoped></style>
