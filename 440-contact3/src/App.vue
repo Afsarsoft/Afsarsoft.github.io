06e5aa7f25baea4cddc1d5edd239722940252985
Afsarsoft <admin@afsarsoft.com>
2022-08-13 14:51:35 -0700

    ...

---
+++ 440-contact3/src/App.vue
@@ -0,0 +1,62 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import ContactInfo from "@/components/ContactInfo.vue";
+// Easier way to do it by changing the vite.config.ts
+// import ContactInfo from "./components/ContactInfo.vue";
+
+export default defineComponent({
+  components: {
+    ContactInfo,
+  },
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 654421 21",
+          email: "julie@localhost.com",
+        },
+        {
+          id: "mark",
+          name: "Mark Brown",
+          phone: "0944 994421 33",
+          email: "mark@localhost.com",
+        },
+      ],
+    };
+  },
+  methods: {},
+});
+</script>
+
+<template>
+  <header>
+    <h1>Contacts</h1>
+  </header>
+
+  <body>
+    <ul>
+      <!-- Note: need to be Kebab case here -->
+      <!-- To have boolean type, we need to use v-bind or just : -->
+      <!-- Note that only string value can have without v-bind  -->
+      <contact-info
+        v-for="friend in friends"
+        :key="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="true"
+      ></contact-info>
+    </ul>
+  </body>
+</template>
+
+<style scoped></style>
---
+++ 440-contact3/src/components/ContactInfo.vue
@@ -0,0 +1,81 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+// COMMENT
+// Not use the below for now, since only works with <script setup lang="ts">
+// and with that bnot able to able to have "defineComponent"
+// defineProps<{
+//   // Need to be Camel case here
+//   id: string;
+//   name: string;
+//   phoneNumber: string;
+//   emailAddress: string;
+// }>();
+
+// https://vuejs.org/guide/typescript/overview.html
+export default defineComponent({
+  props: {
+    id: {
+      type: String,
+      required: false,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      friendIsFavorite: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.friendIsFavorite = !this.friendIsFavorite;
+    },
+  },
+  mounted() {
+    this.id;
+    this.name;
+    this.phoneNumber;
+    this.emailAddress;
+  },
+});
+</script>
+
+<template>
+  <li>
+    <h2>{{ name }} {{ friendIsFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
